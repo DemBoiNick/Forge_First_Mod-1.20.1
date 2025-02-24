38417bd0281a4c3985d087aa17939da4f5eea70b
Nick <nicholassubota@gmail.com>
2025-02-23 19:43:26 -0600

    Changed creative mod tab name

--- src/main/java/net/demboinick/demboimod1/item/ModCreativeModTabs.java
+++ src/main/java/net/demboinick/demboimod1/item/ModCreativeModTabs.java
@@ -15,10 +15,10 @@ public class ModCreativeModTabs {
     public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS =
             DeferredRegister.create(Registries.CREATIVE_MODE_TAB, FirstMods.MOD_ID);
 
-    public static final RegistryObject<CreativeModeTab> TUTORIAL_TAB = CREATIVE_MODE_TABS.register(
-            "tutorial_tab",
+    public static final RegistryObject<CreativeModeTab> DEMBOITAB = CREATIVE_MODE_TABS.register(
+            "demboitab",
             () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModItems.SAPPHIRE.get()))
-                    .title(Component.translatable("creativetab.tutorial_tab"))
+                    .title(Component.translatable("creativetab.demboitab"))
                     .displayItems((pParameters, pOutput) ->{
                         pOutput.accept(ModItems.SAPPHIRE.get());
                         pOutput.accept(ModItems.RAW_SAPPHIRE.get()) ;
